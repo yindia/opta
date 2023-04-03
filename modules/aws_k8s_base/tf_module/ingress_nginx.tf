75c2f6bfd8ba2812bcd3f12ba51dadbbdc7279b5
Jeev B <jeevb@users.noreply.github.com>
2023-04-03 15:04:42 -0700

    Add new EKS K8S versions

--- modules/aws_k8s_base/tf_module/ingress_nginx.tf
+++ modules/aws_k8s_base/tf_module/ingress_nginx.tf
@@ -39,11 +39,14 @@ resource "aws_acm_certificate" "user_provided" {
 
 // NOTE: following this solution for http -> https redirect: https://github.com/kubernetes/ingress-nginx/issues/2724#issuecomment-593769295
 resource "helm_release" "ingress-nginx" {
-  count            = var.nginx_enabled ? 1 : 0
-  chart            = "ingress-nginx"
-  name             = "ingress-nginx"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  version          = contains(["1.19", "1.20", "1.21", "1.22"], var.k8s_version) ? "4.1.4" : "3.40.0"
+  count      = var.nginx_enabled ? 1 : 0
+  chart      = "ingress-nginx"
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  version = contains(
+    ["1.19", "1.20", "1.21", "1.22", "1.23", "1.24", "1.25"],
+    var.k8s_version
+  ) ? "4.1.4" : "3.40.0"
   namespace        = "ingress-nginx"
   create_namespace = true
   atomic           = true
