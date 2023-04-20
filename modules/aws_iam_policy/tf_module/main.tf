049248f1c5e2652f44d7c75a152fd62026f7efdc
Jeev B <jeevb@users.noreply.github.com>
2023-04-19 21:52:31 -0700

    Add support for inferring commonly used variables in aws_iam_policy module

--- modules/aws_iam_policy/tf_module/main.tf
+++ modules/aws_iam_policy/tf_module/main.tf
@@ -1,3 +1,14 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+locals {
+  templatevars = {
+    account_id = data.aws_caller_identity.current.account_id
+    arn        = data.aws_caller_identity.current.arn
+    region     = data.aws_region.current.name
+  }
+}
+
 resource "random_string" "policy_suffix" {
   length  = 4
   special = false
@@ -6,7 +17,7 @@ resource "random_string" "policy_suffix" {
 
 resource "aws_iam_policy" "policy" {
   name   = "${var.env_name}-${var.layer_name}-${var.module_name}"
-  policy = file(var.file)
+  policy = templatefile(var.file, local.templatevars)
   tags = {
     "opta-created" : true
   }
